e8e827536dfd212912c6c6bb40af9e687fff6572
sih <sih@sihs-MacBook-Pro.local>
2025-07-08 21:20:50 +0530

    Modified Code

--- basic/handlingWindows.py
+++ basic/handlingWindows.py
@@ -7,8 +7,6 @@ driver.maximize_window()
 
 # Parent window handle
 
-h3_message = driver.find_element(By.TAG_NAME, "h3").text
-print(f"Message on the page: {h3_message}")
 
 click_here_link = driver.find_element(By.LINK_TEXT, "Click Here")
 click_here_link.click()
@@ -19,3 +17,7 @@ windows = driver.window_handles
 driver.switch_to.window(windows[1])
 new_window_message = driver.find_element(By.TAG_NAME, "h3").text
 print(f"Message in the new window: {new_window_message}")
+
+driver.switch_to.window(windows[0])
+parent_window_message = driver.find_element(By.TAG_NAME, "h3").text
+print(f"Message in the parent window: {parent_window_message}")
