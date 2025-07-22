242d6995895aaf502d88698a2d810effeb1aba9f
sih <sih@sihs-MacBook-Pro.local>
2025-07-22 15:48:44 +0530

    New

--- Random/triangle.py
+++
@@ -1,8 +0,0 @@
-row = 7
-for i in range(row):
-    for j in range(row):
-        if i == j or j == row - i - 1:
-            print("*", end=" ")
-        else:
-            print(" ", end=" ")
-    print()
